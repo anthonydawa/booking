459ee7743ec9f5d7b5bf853897a4c092eae2050e
anthonydawa <anthonydawa0202@gmail.com>
2020-12-03 14:56:57 +0800

    first

---
+++ src/component/login-form/login-form.component.jsx
@@ -0,0 +1,29 @@
+const LoginForm = () => {
+  <div className="cntr">
+    <form>
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleInputPassword1">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  </div>;
+};
+
+export default LoginForm;
